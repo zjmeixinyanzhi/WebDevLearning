bff44053c9d23d00d7754983d9bd162ce857a362
ADMIN-PC <zhangjiewanwansui@gmail.com>
2017-03-01 09:58:05 +0800

    Add image checking-code

---
+++ WebDev/src/com/zjsoft/util/ip/IpUtil.java
@@ -0,0 +1,16 @@
+package com.zjsoft.util.ip;
+
+import com.zjsoft.util.ip.IPSeeker;
+
+public class IpUtil {
+
+	public static String getIpAddress(String ip) {
+		try{
+			return IPSeeker.getInstance().getAddress(ip);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "Î´ÖªÇøÓò";
+	}
+
+}
