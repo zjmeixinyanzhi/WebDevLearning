0d643928687d51e850503a1c96d369fb1dddda14
zhangjie <zhangjie@iscas.ac.cn>
2017-10-12 15:42:42 +0800

    jpa and oauth2 integration

---
+++ Spring/springmvc-jpa-oauth2-security-demo/src/main/java/com/sivalabs/springapp/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.sivalabs.springapp.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
